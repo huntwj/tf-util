396d03777ff3145f44d9eb4bc4bb04b3f06b2f79
Wil Hunt <huntwj@gmail.com>
2015-12-04 22:54:03 -0800

    Add events support
    /util_addListener <event> <callback>
    /util_removeListener <event> <callback>
    /util_fireEvent <event> <parameters>

---
+++ events.tf
@@ -0,0 +1,40 @@
+;;
+;; tf-util/events.tf
+;;
+/loaded __tf_util_events__
+
+/load lisp.tf
+
+/def util_addListener = \
+    /let _event=$[textencode({1})]%;\
+    /let _callback=$[textencode({2})]%;\
+    /let _tmp=%;\
+    /test _tmp := strcat(util_event_%{_event}, ' ', _callback)%;\
+    /set util_event_%{_event}=$(/unique %{_tmp})
+
+/def util_removeListener = \
+    /let _event=$[textencode({1})]%;\
+    /let _callback=$[textencode({2})]%;\
+    /let _tmp=%;\
+    /test _tmp := util_event_%{_event}%;\
+    /let _newVal=$(/remove %{_callback} %{_tmp})%;\
+    /if (regmatch("^\s*$", _newVal)) \
+        /unset util_event_%{_event}%;\
+    /else \
+        /set util_event_%{_event}=%{_newVal}%;\
+    /endif
+
+/def util_fireEvent = \
+    /let _event=$[textencode({1})]%;\
+    /let _params=%{-1}%;\
+    /let _list=%;\
+    /test _list := util_event_%{_event}%;\
+    /let _count=$(/length %{_list})%;\
+    /while (_count > 0) \
+        /let _callback=$(/car %{_list})%;\
+        /let _list=$(/cdr %{_list})%;\
+        /test _callback := textdecode(_callback)%;\
+        /%{_callback} %{_params}%;\
+        /let _count=$(/length %{_list})%;\
+    /done
+
---
+++ main.tf
@@ -0,0 +1,8 @@
+;;
+;; tf-util/main.tf
+;;
+/loaded __tf_util_main__
+
+/require tf-util/variables.tf
+/require tf-util/events.tf
+
