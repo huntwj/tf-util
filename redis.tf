83c5e759b81eea52946f5d63bb5bc435d1ff5dfc
Wil Hunt <huntwj@gmail.com>
2016-12-21 02:20:49 -0800

    Add more commands to redis support.

--- redis.tf
+++ redis.tf
@@ -5,16 +5,43 @@
 ;
 /loaded __tf_util_redis__
 
+/def -q redisEscape = \
+    /let _val=%{1}%;\
+    /test _val := strcat(char(34), _val, char(34))%;\
+    /result _val%;\
+
 /def -q redisCmd = \
+;    /echo Params: %{-1}%;\
     /let _result=$(/quote -S -decho !redis-cli %{*})%;\
     /result _result
 
 /def redisGet = \
-    /result redisCmd("get", {1})
+    /result redisCmd("GET", {1})
+
+/def redisLIndex = \
+    /result redisCmd("LINDEX", {1}, {2})
+
+/def redisLLen = \
+    /result redisCmd("LLEN", {1})
 
 /def redisSet = \
-    /result redisCmd("set", {1}, {2})
+    /result redisCmd("SET", {1}, redisEscape({2}))
 
 /def redisIsSet = \
-    /result redisCmd("exists", {1})
+    /result redisCmd("EXISTS", {1})
+
+/def redisLPush = \
+    /result redisCmd("LPUSH", {1}, redisEscape({2}))
+
+/def redisRPush = \
+    /result redisCmd("RPUSH", {1}, redisEscape({2})
+
+/def redisLRange = \
+    /result redisCmd("LRANGE", {1}, {2}, {3--1})
+
+/def redisDel = \
+    /result redisCmd("DEL", {1})
+
+/def redisRename = \
+    /result redisCmd("RENAME", {1}, {2})
 
